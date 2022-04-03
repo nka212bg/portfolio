b777a4d93074dad7d942860ec567c50035d85311
nka212bg <nka212bg@gmail.com>
2022-04-03 06:50:14 +0000

    new item cards

--- index.php
+++ index.php
@@ -3,167 +3,48 @@ require 'components/header.php';
 require 'components/headerMenu.php';
 ?>
 
-
-
 <div v-scope='{ 
-    popupFor : "Dev", 
+    items: <?= file_get_contents("items/items.json") ?>,
+    common: <?= file_get_contents("items/common.json") ?>,
+    selectedItem: null,
+    filterTag : null,
+    filterBy(val){
+        const items = JSON.parse(JSON.stringify(this.items));
+        items.forEach(e => val && !e.tags.includes(val) && e.category != val ? (e.hide = true) : delete e.hide);
+        this.items = items;
+        this.filterTag = val;
+    },   
 }'>
 
 
-    <div class="app-header-menu__right__social-link bigger--font-size text--secondary-8" @click="popupFor = 'Dev'">Dev</div>
+    <div class="m-6 p-6 flex-center flex-middle">Something, something</div>
 
-    <div class="app-header-menu__right__social-link bigger--font-size text--secondary-8" @click="popupFor = 'design'">design</div>
+    <!-- ITEMS -->
+    <div style="position: relative;">
+        <div v-if="filterTag" class="flex-center smaller--font-size link text--white" style="position: absolute; top: 0; left: 33px" @click="filterBy(null)"><span :style="{color: common.tags[filterTag].color, fontFamily: 'var(--font-logos)' }" class="small--font-size mr-05">{{common.tags[filterTag].icon}}</span> filtered by {{filterTag}}</div>
+        <div class="home-portfolio p-2">
+            <div v-for="(item, i) in items" :key="i" v-show="!item.hide" class="portfolio-item" @click="selectedItem = item">
+                <div class="img" :style="`background-image: url('${item.img}')`"></div>
+                <div class="portfolio-item-content">
+                    <div class="item-title">{{item.title}} {{item.hide}}</div>
+                    <div class="item-description">{{item.description}}</div>
+                    <div class="item-bottom">
+                        <div class="item-category" @click.stop="filterBy(item.category)">{{item.category}}</div>
+                        <div class="item-tags">
+                            <span v-for="(tag, t) in item.tags" :key="t" class="item-tags-tag" :title="tag" :style="{color: common.tags[tag].color}" @click.stop="filterBy(tag)">{{common.tags[tag].icon}}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-
-
-
-    <div v-if="popupFor" class="popup-view">
+    <div v-if="selectedItem" class="popup-view">
         <div class="popup-view__container popup-view__container--fullWidth">
             <!-- TOP -->
-            <div class="popup-view__container__close" @click="popupFor = false">&times;</div>
-            <h1 class="ml-1 mt-05">{{popupFor}}</h1>
-            <template v-if="popupFor === 'Dev'">
-                <div class="popup-view__container-content">
-
-                    <div class="portfolio-holder">
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 1</div>
-                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 2</div>
-                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 1</div>
-                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 2</div>
-                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 1</div>
-                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 2</div>
-                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 1</div>
-                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="portfolio-holder__item">
-                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
-                            <div class="portfolio-holder__item-content">
-                                <div class="item-title">Item title text 2</div>
-                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
-                                <div class="item-bottom">
-                                    <div class="item-category">Design</div>
-                                    <div class="item-tags">
-                                        <span></span>
-                                        <span></span>
-                                        <span></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-
-                    </div>
-
-
-                </div>
-            </template>
-            <template v-if="popupFor === 'design'">
-                <h1>{{popupFor}}</h1>
-            </template>
+            <div class="popup-view__container__close" @click="selectedItem = null">&times;</div>
+            <h1 class="ml-1 mt-05">{{selectedItem}}</h1>
         </div>
     </div>
 
---
+++ index____.php
@@ -0,0 +1,206 @@
+<?php
+require 'components/header.php';
+require 'components/headerMenu.php';
+?>
+
+
+
+<div v-scope='{ 
+    popupFor : false, 
+}'>
+
+
+    <div class="app-header-menu__right__social-link bigger--font-size text--secondary-8" @click="popupFor = 'Dev'">Dev</div>
+    <div class="app-header-menu__right__social-link bigger--font-size text--secondary-8" @click="popupFor = 'design'">design</div>
+    <!-- //////////////// -->
+
+    <div class="portfolio-item">
+                <div class="img" style="background-image: url('/img/door2.jpg')"></div>
+                <div class="portfolio-item-content">
+                    <div class="item-title">Item title text 1</div>
+                    <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                    <div class="item-bottom">
+                        <div class="item-category">Design</div>
+                        <div class="item-tags">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="portfolio-item">
+                <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
+                <div class="portfolio-item-content">
+                    <div class="item-title">Item title text 2</div>
+                    <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt. </div>
+                    <div class="item-bottom">
+                        <div class="item-category">Design</div>
+                        <div class="item-tags">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+    <!-- ////////////// -->
+
+
+
+
+    <div v-if="popupFor" class="popup-view">
+        <div class="popup-view__container popup-view__container--fullWidth">
+            <!-- TOP -->
+            <div class="popup-view__container__close" @click="popupFor = false">&times;</div>
+            <h1 class="ml-1 mt-05">{{popupFor}}</h1>
+            <template v-if="popupFor === 'Dev'">
+                <div class="popup-view__container-content">
+
+                    <div class="portfolio-holder">
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 1</div>
+                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 2</div>
+                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt.Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried excepttt. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 1</div>
+                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 2</div>
+                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 1</div>
+                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 2</div>
+                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/door2.jpg')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 1</div>
+                                <div class="item-description">Feelings potatoes break cats minute carried newspaper rear. Stick early excitement being better cucumber loaf scuttered, reaching hung. Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="portfolio-holder__item">
+                            <div class="img" style="background-image: url('/img/items/Capture.PNG')"></div>
+                            <div class="portfolio-holder__item-content">
+                                <div class="item-title">Item title text 2</div>
+                                <div class="item-description">Clothes front railroad assertion started wait angels. Telegram fortunately squeeze ragged brown, charge bending. Together thought altogether carrying elusive lips hurt tried except. </div>
+                                <div class="item-bottom">
+                                    <div class="item-category">Design</div>
+                                    <div class="item-tags">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+
+                </div>
+            </template>
+            <template v-if="popupFor === 'design'">
+                <h1>{{popupFor}}</h1>
+            </template>
+        </div>
+    </div>
+
+</div>
+
+<?php require 'components/footer.php' ?>
