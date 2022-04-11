cb99d7d902c27e6793df0207f533817bb0a60965
nka212bg <nka212bg@gmail.com>
2022-04-11 22:26:11 +0000

    init

--- components/header.php
+++ components/header.php
@@ -8,7 +8,7 @@ $t = $i18n["language"];
 <html lang="<?= $i18n["selectedLanguage"] ?>" style='background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.9)), url("/items/img/bg_<?= rand(1, 8) ?>.jpg");'>
 
 <head>
-    <title><?= isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['title'] ?></title>
+    <title><?= (isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['myName'] . " | " . $t['title']) . " | " .  date("Y m") ?></title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,13 +20,13 @@ $t = $i18n["language"];
 
 
     <!-- social media -->
-    <meta property="og:title" content="<?= isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['title'] ?>" />
-    <meta property="og:site_name" content="<?= isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['title'] ?>" />
+    <meta property="og:title" content="<?= (isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['myName'] . " | " . $t['title'])  . " | " .  date("Y m") ?>" />
+    <meta property="og:site_name" content="<?= (isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['myName'] . " | " . $t['title']) . " | " .  date("Y m") ?>" />
     <meta property="og:type" content="website" />
     <meta property="og:description" content="<?= $meta_description ?? $t['meta_description'] ?>" />
     <meta property="og:image" content="<?= "$netProtocol://$_SERVER[HTTP_HOST]/" . ($meta_social_img ?? 'items/img/bg_' . rand(1, 8) . '.jpg') ?>" />
     <meta property="og:url" content="<?= "$netProtocol://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>" />
-    <meta name="twitter:title" content="<?= isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['title'] ?>" />
+    <meta name="twitter:title" content="<?= (isset($meta_title) ? $meta_title . " | " . $t['title']  : $t['myName'] . " | " . $t['title']) . " | " .  date("Y m") ?>" />
     <meta name="twitter:description" content="<?= $meta_description ?? $t['meta_description'] ?>" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@www.<?= "$_SERVER[HTTP_HOST]" ?>" />
@@ -46,10 +46,10 @@ $t = $i18n["language"];
 
     <!-- SCRIPTS -->
     <!-- https://vuejsexamples.com/petite-vue-6kb-subset-of-vue-optimized-for-progressive-enhancement/  petit vue tutorial -->
-    <script src="script/petite-vue.js" defer init></script>    
+    <script src="script/petite-vue.js" defer init></script>
     <!-- <script src="script/shuffle.js"></script> -->
     <!-- <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script> -->
-    
+
     <script src="script/main.js"></script>
     <script src="https://nkatanasov.com/analitics/analitics.js" defer></script>
 
--- utils/locale/langs/en.php
+++ utils/locale/langs/en.php
@@ -17,6 +17,6 @@ return [
     "description" => "It's never about what it is, but how you gonna present it!",
 
     /// meta
-    "meta_description" => "Nikolay Krasimirov Atanasov portfolio website",
+    "meta_description" => "Nik Atanasov portfolio website. Punchayet shook fright reaching, crime perhaps wander unfortunately. Quite should year stayed parted exert girl yesterday climbed, slipped pale. Dust howler toolshed account paralyzed ragged. Triumph soft also double want revealed.  Carry spent rake sand beginning puffing abandoned beginning became. Belong piled after went flopsy relaxed anything intelligence moment taken breath. Stop pair striving toolshed room doorstep cushion carefully unfortunately. Room tail pond names, idleness peas grief count palaver toolshed must. Jumped wrapped being else, intelligence names heaven talking besides went. That husband above bundle same find, humphed carrying sorts.  Abandoned true disaster what welcome hump came indaba throat untouched dull. Help glad boots besides prickles really blaze sorts help, someone.",
     "meta_keywords" => "portfolio, personal page",
 ];
